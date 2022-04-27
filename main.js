88ab12c8ef0f54cead913ca3cc2aa69809a09cf0
sunny2317 <48347696+sunny2317@users.noreply.github.com>
2022-04-27 16:31:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+/* jquery */
+
+$(document).ready(function(){
+  $("p").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+  });
+});
+
